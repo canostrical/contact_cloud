9bbe7f7637aba82c6169b427eb09c7e4e21e0972
jraedisch <jasper@raedisch.net>
2023-08-11 13:39:58 +0200

    extract functions

--- chart_d3.js
+++ chart_d3.js
@@ -7,6 +7,47 @@ import { color } from "./vendor/d3-color-3-1-0.js";
 import { zoom, zoomIdentity } from "./vendor/d3-zoom-3-0-0.js";
 import { bech32, base16 } from "./vendor/scure-base-1-1-1.js";
 
+const drawPoint = (context, scaleX, scaleY, point, colors, selected) => {
+  context.beginPath();
+  const [color, size, alpha] = selected
+    ? ["#000000", 10, 1]
+    : [colors[point[2]], 5, 0.75];
+  context.globalAlpha = alpha;
+  context.fillStyle = color;
+  const px = scaleX(point[0]);
+  const py = scaleY(point[1]);
+
+  context.arc(px, py, size, 0, 2 * Math.PI, true);
+  context.fill();
+};
+
+const closestPoint = (points, x, y, eventX, eventY, radius) => {
+  let closestPoint;
+  let minDistance = Infinity;
+  points.forEach((point) => {
+    const pointX = x(point[0]);
+    const pointY = y(point[1]);
+    const distance = Math.hypot(pointX - eventX, pointY - eventY);
+    if (distance < minDistance && radius != null && distance < radius) {
+      closestPoint = point;
+      minDistance = distance;
+    }
+  });
+
+  return closestPoint;
+};
+
+const bechToHex = (npub) =>
+  base16.encode(bech32.fromWords(bech32.decode(npub).words)).toLowerCase();
+
+const hexToBech = (hex) =>
+  bech32.encode("npub", bech32.toWords(base16.decode(hex.toUpperCase())));
+
+const pubkeyValueAsHex = (input) => {
+  const pubkey = input.value.trim().toLowerCase();
+  return pubkey.startsWith("npub") ? bechToHex(pubkey) : pubkey;
+};
+
 const run = async () => {
   let colors = [];
   const points = [];
@@ -29,20 +70,6 @@ const run = async () => {
   const x = scaleLinear().domain([-1.0, 1.0]).range([0, width]);
   const y = scaleLinear().domain([-1.0, 1.0]).range([height, 0]);
 
-  const drawPoint = (scaleX, scaleY, point, selected) => {
-    context.beginPath();
-    const [color, size, alpha] = selected
-      ? ["#000000", 10, 1]
-      : [colors[point[2]], 5, 0.75];
-    context.globalAlpha = alpha;
-    context.fillStyle = color;
-    const px = scaleX(point[0]);
-    const py = scaleY(point[1]);
-
-    context.arc(px, py, size, 0, 2 * Math.PI, true);
-    context.fill();
-  };
-
   const draw = (transform) => {
     const scaleX = transform.rescaleX(x);
     const scaleY = transform.rescaleY(y);
@@ -50,10 +77,12 @@ const run = async () => {
     context.clearRect(0, 0, width, height);
 
     points.forEach((point) => {
-      if (selectedPoint !== point) drawPoint(scaleX, scaleY, point, false);
+      if (selectedPoint !== point)
+        drawPoint(context, scaleX, scaleY, point, colors, false);
     });
 
-    if (selectedPoint) drawPoint(scaleX, scaleY, selectedPoint, true);
+    if (selectedPoint)
+      drawPoint(context, scaleX, scaleY, selectedPoint, colors, true);
   };
 
   const csv = await text("nostr_graph.csv");
@@ -80,17 +109,6 @@ const run = async () => {
 
   const input = document.getElementById("pubkey");
 
-  const bechToHex = (npub) =>
-    base16.encode(bech32.fromWords(bech32.decode(npub).words)).toLowerCase();
-
-  const hexToBech = (hex) =>
-    bech32.encode("npub", bech32.toWords(base16.decode(hex.toUpperCase())));
-
-  const pubkeyValueAsHex = () => {
-    const pubkey = input.value.trim().toLowerCase();
-    return pubkey.startsWith("npub") ? bechToHex(pubkey) : pubkey;
-  };
-
   const zoomTo = (point) => {
     console.log(`zooming to ${point}`);
     selectedPoint = point;
@@ -101,7 +119,7 @@ const run = async () => {
 
   const processPubkey = () => {
     try {
-      const pointI = hexs.indexOf(pubkeyValueAsHex());
+      const pointI = hexs.indexOf(pubkeyValueAsHex(input));
 
       if (pointI > -1) {
         zoomTo(points[pointI]);
@@ -111,6 +129,12 @@ const run = async () => {
     }
   };
   input.addEventListener("change", processPubkey);
+
+  node.addEventListener("mousemove", (e) => {
+    const closest = closestPoint(points, x, y, e.clientX, e.clientY, 10);
+    if (closest) {
+    }
+  });
 };
 
 run();
