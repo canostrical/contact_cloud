a18cb7f91faf453d8de632a01510e33893cb34db
jraedisch <jasper@raedisch.net>
2023-10-12 17:03:26 +0200

    fix nostr links

--- chart_d3.js
+++ chart_d3.js
@@ -9,7 +9,7 @@ import { bech32, base16 } from "./vendor/scure-base-1-1-1.js";
 
 // using nostr links until nostrapp.link or alternative works reliably on (my) iPhone
 const isIOS = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-const urlPrefix = isIOS ? "nostr:" : "https://nostrapp.link/#";
+const urlPrefix = isIOS ? "nostr:" : "https://nostrapp.link/main/apps/social#";
 
 const href = document.getElementById("href");
 
